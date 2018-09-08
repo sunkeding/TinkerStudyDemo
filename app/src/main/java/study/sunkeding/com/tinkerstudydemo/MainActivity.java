1f6db1741c9ec2863fdf1ca24a2ef33c215f9201
keding <keding@leoao.com>
2018-09-09 01:41:08 +0800

    打出0.0.1release版本，该版本点击第一个按钮会crash

--- app/src/main/java/study/sunkeding/com/tinkerstudydemo/MainActivity.java
+++ app/src/main/java/study/sunkeding/com/tinkerstudydemo/MainActivity.java
@@ -3,7 +3,7 @@ package study.sunkeding.com.tinkerstudydemo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.tinkerpatch.sdk.TinkerPatch;
@@ -18,19 +18,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initListener() {
-        final Button bt1 = findViewById(R.id.bt1);
-        bt1.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.bt1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                bt1.setText("fix 空指针");
-
+                TextView tv = null;
+                tv.setText("d");
             }
         });
         findViewById(R.id.bt2).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 TinkerPatch tinkerPatch = TinkerPatch.with().fetchPatchUpdate(true);
-                if (tinkerPatch != null) {
+                if (tinkerPatch!=null){
                     Toast.makeText(MainActivity.this, "tinkerPatch.getPatchVersion():" + tinkerPatch.getPatchVersion(), Toast.LENGTH_SHORT).show();
                 }
             }
